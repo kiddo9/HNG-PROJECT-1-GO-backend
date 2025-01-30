ee892b8e7a6cd5f2a6adb6eec2463bc452cd7315
kiddo9 <dkido913@gmail.com>
2025-01-30 17:39:38 +0100

    tenth commit

--- main.go
+++ main.go
@@ -10,9 +10,6 @@ import (
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
-		// w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
 		next.ServeHTTP(w, r)
 	})
 }
