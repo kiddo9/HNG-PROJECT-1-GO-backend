4de6a78eca3167803d6469fbf3024a1ab22d0d97
kiddo9 <dkido913@gmail.com>
2025-01-30 18:24:33 +0100

    19th commit

--- models/jsonStruct.go
+++ models/jsonStruct.go
@@ -3,6 +3,6 @@ package models
 // UserResponse represents the structure of our JSON response
 type UserResponse struct {
 	Email       string `json:"email"`
-	CurrentTime string `json:"current_dateTime"`
+	CurrentTime string `json:"current_datetime"`
 	GitHubURL   string `json:"github_url"`
 }
