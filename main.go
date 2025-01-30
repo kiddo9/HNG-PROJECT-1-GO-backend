66a44c4b1b9bae2e555c35a852a8e9342a7e6a90
kiddo9 <dkido913@gmail.com>
2025-01-30 17:36:09 +0100

    tenth commit

--- main.go
+++ main.go
@@ -13,28 +13,9 @@ func corsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
 		next.ServeHTTP(w, r)
 	})
 }
-
-// Request handler
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.Method {
-// 	case "GET":
-// 		fmt.Fprintln(w, "This is a GET request")
-// 	case "POST":
-// 		fmt.Fprintln(w, "This is a POST request")
-// 	case "PATCH":
-// 		fmt.Fprintln(w, "This is a PATCH request")
-// 	default:
-// 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-// 	}
-// }
 
 func main() {
 	mux := http.NewServeMux()
